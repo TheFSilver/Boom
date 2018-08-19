7fd7747a4d38fba4bfa2dec1a7edab651df01ed0
TheFSilver <dasylva.f@gmail.com>
2018-08-19 03:18:50 +0200

    template update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= js/custom/jquery-3.2.1.min
+//= js/custom/popper.min
+//= js/bootstrap/bootstrap.min
+//= assets/js/custom/plugins.min
+//= js/custom/custom.min
